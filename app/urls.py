9203ae1211a73abd7b989a69225186281a1aa01d
Haroldo Alcobaças <haroldo.alcaboacas@gmail.com>
2024-02-24 12:26:19 -0300

    tela login e logout

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.login, name = "login"),
-    path("cadastro_usuario/",views.cadastro_usuario, name="cadastro_usuario"),
-    path("salvar_dados/",views.salvar_dados, name="salvar_dados"),
+    path("",views.home, name = "home"),
+    path("cadastro_usuario/",views.cadastro_usuario, name='cadastro_usuario'),
+    path("salvar_dados/",views.salvar_dados, name='salvar_dados'),
+    path("tela_login/",views.tela_login, name='tela_login'),
+    path("process_login/",views.process_login, name='process_login'),
+    path("dashboard/", views.dashboard, name='dashboard'),
+    path("logouts/", views.logouts, name='logouts'),
+    path("password/", views.changePassowrd, name='password'),
 
    
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
 
 # Create your views here.
 
-def login(request):
+def home(request):
     return render(request,'home.html')
 
 def cadastro_usuario(request):
@@ -14,11 +18,43 @@ def salvar_dados(request):
     data = {}
     if(request.POST['password'] != request.POST['password-conf']):
         data['msg'] = 'Senha e confirmação de senha diferentes!'
-        data['class'] = 'alert-danger'
+        data['class'] = 'alert-danger' 
     else:
-        user = User.objects.create_user(request.POST['user'],request.POST['email'] , "request.POST['password']")
+        user = User.objects.create_user(request.POST['user'],request.POST['email'] , request.POST['password'])
         user.first_name = request.POST['name']
         user.save()
         data['msg'] = 'Usuário Cadastrado com Sucesso!'
         data['class'] = 'alert-success'      
-    return render(request, 'cadastro_usuario.html', data)
+    return render(request, 'cadastro_usuario.html',data)
+
+
+def dashboard(request):
+    return render(request,'dashboard/home.html')
+
+def tela_login(request):
+    return render(request,'tela_login.html')
+
+def process_login(request):   
+        
+        user = authenticate(username=request.POST['user'], password=request.POST['password'])
+        data = {}
+        if user is not None:           
+            login(request, user)
+            return redirect('/dashboard/')
+            
+        else:
+            data['msg'] = 'Usuário ou Senha Incorretas!'
+            data['css'] = 'alert-danger' 
+            return render(request,'tela_login.html',data)
+    
+    
+def logouts(request):
+    logout(request)
+    return redirect('/tela_login/')
+
+def changePassowrd(request):
+    user = User.objects.get(email=request.user.email)
+    user.set_password(request.POST['password'])
+    user.save()
+    logout(request)
+    return redirect('/tela_login/')
